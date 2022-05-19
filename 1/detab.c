d2b71f720d1ab95d96df21d38c7df0f120852c73
Park John <yjpark@ucdavis.edu>
2022-05-19 15:20:59 -0700

    initial commit

---
+++ 1/detab.c
@@ -0,0 +1,14 @@
+/*
+ * k&r exercise 1-20 p.34 
+ * Write a program detab that replaces tabs in the input with the proper
+ * number of blanks to space to the next tab stop. Assume a fixed set of
+ * tab stops, say every n columns. Should n be a variable or a symbolic 
+ * parameter? 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
