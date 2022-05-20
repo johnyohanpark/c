016aaa7143d71b7b309fb57621870d75bdda405d
Park John <yjpark@ucdavis.edu>
2022-05-20 03:13:37 -0700

    inital commit

---
+++ 1/fold.c
@@ -0,0 +1,16 @@
+/*
+ * from k&r exercise 1-22 p.34 Write a program to "fold" long input
+ * lines into two or more shorter lines after the last non-blank
+ * character that occurs before the n-th column of input. Make sure
+ * your program does something intelligent with very long lines,
+ * and if there are no blanks or tabs before the specified column. 
+ *
+ */
+
+#include <stdio.h>
+#define TOOLONG 20
+
+int main()
+{
+    
+}
