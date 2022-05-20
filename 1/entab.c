f805df1146d4d27836017420e3924971ebe45448
Park John <yjpark@ucdavis.edu>
2022-05-20 00:00:20 -0700

    entab inital commit

---
+++ 1/entab.c
@@ -0,0 +1,14 @@
+/*
+ * k&r exercise 1-21 p.34 Write a program "entab" that replaces strings of blanks by
+ * the minimum number of tabs and blanks to achieve the same spacing. Use the same
+ * tab stops as for "detab". When either a tab or single blank would suffice to reach
+ * a tab stop, which should be given preference?
+ */
+
+#include <stdio.h>
+
+int main()
+{
+
+    return 0;
+}
